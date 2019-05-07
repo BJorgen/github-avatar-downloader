317e7fbf9eb98bc8fbd004ee476ce2610fe10db8
Britta Jorgenson <bljorgen@gmail.com>
2019-05-07 02:24:42 +0000

    Added and called getRepoContributors() function.

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,11 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, cb) {
+    let url = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+    
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+});
