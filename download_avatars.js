1b97977a9ae63cbd639f69473cd021835b300013
Britta Jorgenson <bljorgen@gmail.com>
2019-05-07 02:54:33 +0000

    Added authentication with github token in secrets.js file.

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,23 @@
 var request = require('request');
+var MY_TOKEN = require('./secrets').GITHUB_TOKEN;
+console.log(MY_TOKEN);
+
+console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    let url = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
     
+    var options = {
+        url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': 'token ' + MY_TOKEN
+        }
+    }
+
+    request(options, function(err, res, body) {
+        cb(err, body);
+    });
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
